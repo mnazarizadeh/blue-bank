67cfb69834aa5dd745dbad0884291f81e0b7236b
Mohammad Nazarizadeh <m.nazarizade@mydigipay.com>
2022-08-25 15:56:31 +0430

    initial commit

---
+++ account-service/account-mng/src/main/java/com/example/accountmng/AccountMngApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMngApplication.class, args);
+	}
+
+}
---
+++ transaction-service/transaction-mng/src/main/java/com/example/transactionmng/TransactionMngApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMngApplication.class, args);
+	}
+
+}
