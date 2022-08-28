760368f8ce8dfa222c23e075aa2e30755347a5cc
Mohammad Nazarizadeh <m.nazarizade@mydigipay.com>
2022-08-29 00:28:39 +0430

    AccountClient implemented

---
+++ transaction-service/transaction-mng/src/main/java/com/example/transactionmng/client/account/AccountClient.java
@@ -0,0 +1,26 @@
+package com.example.transactionmng.client.account;
+
+import com.example.accountmng.spec.request.UpdateBalanceRequest;
+import com.example.accountmng.spec.response.AccountBalanceResponse;
+import com.example.common.exception.ClientException;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Component
+@FeignClient(name = "accountClient", url = "${account.base.url}")
+public interface AccountClient {
+
+	@GetMapping(path = "/api/accounts/{accountIdentifier}/balance", produces = MediaType.APPLICATION_JSON_VALUE)
+	AccountBalanceResponse getBalance(@PathVariable String accountIdentifier) throws ClientException;
+
+	@PostMapping(path = "/api/accounts/{accountIdentifier}/balance", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	AccountBalanceResponse updateBalance(@RequestBody UpdateBalanceRequest request) throws ClientException;
+
+}
---
+++ transaction-service/transaction-mng/src/main/java/com/example/transactionmng/client/account/impl/AccountClientAspect.java
@@ -0,0 +1,37 @@
+package com.example.transactionmng.client.account.impl;
+
+import com.example.common.exception.ClientException;
+import com.example.transactionmng.util.ClientExceptionUtils;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class AccountClientAspect {
+
+	@Before("within(com.example.transactionmng.client.account.Account*Client+) & execution(*)")
+	public void init(JoinPoint joinPoint) {
+		log.debug("invoke {} - {}", joinPoint.getSignature().getName(), joinPoint.getArgs()[0]);
+	}
+
+	@AfterReturning(pointcut = "within(com.example.transactionmng.client.account.Account*Client+) & execution(*)",
+			returning = "response")
+	public void verify(Object response) {
+		log.debug("received {}", response);
+	}
+
+	@AfterThrowing(pointcut = "within(com.example.transactionmng.client.account.Account*Client+) & execution(*)", throwing = "ex")
+	public void handle(FeignException ex) throws ClientException {
+		log.error("error occurred: ", ex);
+		throw ClientExceptionUtils.deliver(ex);
+	}
+
+}
