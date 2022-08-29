bdf713ceb4dd1fa5ddbc50eb1304899316cb5039
Mohammad Nazarizadeh <m.nazarizade@mydigipay.com>
2022-08-30 02:40:48 +0430

    update configs

--- transaction-service/transaction-mng/src/main/java/com/example/transactionmng/TransactionMngApplication.java
+++ transaction-service/transaction-mng/src/main/java/com/example/transactionmng/TransactionMngApplication.java
@@ -2,8 +2,14 @@ package com.example.transactionmng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class TransactionMngApplication {
 
 	public static void main(String[] args) {
