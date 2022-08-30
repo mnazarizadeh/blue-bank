4282a70a250427b4006411b723791c9b3926870c
Mohammad Nazarizadeh <m.nazarizade@mydigipay.com>
2022-08-30 12:26:13 +0430

    add swagger for transaction service

---
+++ transaction-service/transaction-mng/src/main/java/com/example/transactionmng/config/SpringDocConfig.java
@@ -0,0 +1,25 @@
+package com.example.transactionmng.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@RequiredArgsConstructor
+class SpringDocConfig {
+
+	private final Environment env;
+
+	@Bean
+	@Profile( { "local", "dev" } )
+	public OpenAPI newOpenAPI() {
+		return new OpenAPI().addServersItem(new Server()
+				.url("/" + env.getProperty("spring.application.name") + env.getProperty("spring.mvc.servlet.path")));
+	}
+
+}
